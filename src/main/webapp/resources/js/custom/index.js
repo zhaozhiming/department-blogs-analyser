80c2c4fbaccd09ebd5ba9ddda427bf6c07fbb993
zhaozhiming <kingzzm1982@sina.com>
2014-06-02 11:02:45 +0800

    remove sidebar.js

--- src/main/webapp/resources/js/custom/index.js
+++ src/main/webapp/resources/js/custom/index.js
@@ -1,5 +1,13 @@
 var dba_app = angular.module('dba', ['ngRoute']);
 
+dba_app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/search', { templateUrl: 'resources/pages/search.html', controller: SearchController})
+        .when('/statistics', { templateUrl: 'resources/pages/statistics.html', controller: StatisticsController})
+        .when('/top', { templateUrl: 'resources/pages/top.html', controller: TopController})
+        .otherwise({redirectTo: '/home'});
+}]);
+
 $(document).ready(function () {
     $('#sideBarMenu').click(function () {
         $('.ui.sidebar').sidebar('toggle');
--- src/main/webapp/resources/js/custom/sidebar.js
+++
@@ -1,8 +0,0 @@
-dba_app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-        .when('/search', { templateUrl: 'resources/pages/search.html', controller: SearchController})
-        .when('/statistics', { templateUrl: 'resources/pages/statistics.html', controller: StatisticsController})
-        .when('/top', { templateUrl: 'resources/pages/top.html', controller: TopController})
-        .otherwise({redirectTo: '/home'});
-}]);
-
